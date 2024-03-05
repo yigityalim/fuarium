b5d3ef7fc9d4b03b3ee6010fc25a76598ce5afe8
yigityalim <mehmetyalm@icloud.com>
2024-03-05 09:12:48 +0300

    klasör yapısı değiştirildi.

--- components/Footer/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Footer(): React.ReactElement {
-    return <footer className='container mx-auto px-4 py-8 sm:px-6 lg:px-8'>Footer</footer>
-}
--- middleware.ts
+++
@@ -1,5 +0,0 @@
-export async function middleware() {}
-
-export const config = {
-    matcher: ['/((?!.*\\.).*)', '/favicon.ico'],
-}
