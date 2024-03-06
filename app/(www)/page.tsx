34dd93a7ae3846c66c07c89fdd60f8c352c3815f
yigityalim <mehmetyalm@icloud.com>
2024-03-06 11:08:45 +0300

    vercel hata düzeltme

--- app/(www)/page.tsx
+++ app/(www)/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Container } from '@/components/Containers'
-import { VizaImage } from '@/lib/visa'
 export default async function Home(): Promise<React.ReactElement> {
     return (
         <Container>
--- env.ts
+++ env.ts
@@ -5,7 +5,7 @@ import { z } from 'zod'
 export const env = createEnv({
     isServer: typeof window === 'undefined',
     client: {
-        NEXT_PUBLIC_BASE_URL: z.string().url(),
+        NEXT_PUBLIC_BASE_URL: z.string(),
     },
     runtimeEnv: {
         NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
