e91ba3e54a44f0ffc1c24333c344403b6fe81248
yigityalim <mehmetyalm@icloud.com>
2024-03-06 11:09:42 +0300

    vercel hata düzeltme 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
+/*
 import createJiti from 'jiti'
 
 const jiti = createJiti(new URL(import.meta.url).pathname)
 
 jiti('./env.ts')
+ */
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
